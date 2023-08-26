56dcff33c11d68ecc6417f2f23706d96750bc2a3
GrantLicata <grant.licata@gmail.com>
2023-08-26 09:05:07 -0700

    Adding back the API routes

--- app/api/contact/[id]/route.js
+++ app/api/contact/[id]/route.js
@@ -3,7 +3,45 @@ import Contact from "@/models/Contact";
 import { NextResponse } from "next/server";
 
 // API call: Delete the specified contact
+export async function DELETE(request, { params }) {
+  const { id } = params;
+
+  try {
+    await connectDB();
+    await Contact.findByIdAndDelete(id);
+
+    return new NextResponse("Post has been deleted", { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+}
 
 // API call: Retrieve a specified contact
+export const GET = async (request, { params }) => {
+  const { id } = params;
+
+  try {
+    await connectDB();
+
+    const contact = await Contact.findById(id);
+
+    return new NextResponse(JSON.stringify(contact), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
 
 // Update a specified contact
+export async function PUT(request, { params }) {
+  const { id } = params;
+  const { firstName, lastName, email, phone, address } = await request.json();
+  await connectDB();
+  await Contact.findByIdAndUpdate(id, {
+    firstName,
+    lastName,
+    email,
+    phone,
+    address,
+  });
+  return new NextResponse("Post has been updated", { status: 200 });
+}
--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -3,5 +3,30 @@ import Contact from "@/models/Contact";
 import { NextResponse } from "next/server";
 
 // API call: Send new contact to the database
+export async function POST(req) {
+  const { firstName, lastName, email, phone, address } = await req.json();
+
+  try {
+    await connectDB();
+    await Contact.create({ firstName, lastName, email, phone, address });
+
+    return NextResponse.json({
+      msg: ["Message sent successfully"],
+      success: true,
+    });
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+}
 
 // API call: Retrieve all contacts in reverse order
+export async function GET() {
+  try {
+    await connectDB();
+    const response = await Contact.find({}).sort({ updatedAt: -1 });
+
+    return NextResponse.json(response);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+}
